8c3d11c4e761e9f0339f28da41cadaec3e14db61
Camilia El Moustarih <camilia.elmoustarih@gmail.com>
2025-02-11 12:30:17 -0500

    Corrections et ajout de musique

--- Assets/_MAIN/Scripts/CliquerObjet3D.cs
+++ Assets/_MAIN/Scripts/CliquerObjet3D.cs
@@ -1,18 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CliquerObjet3D : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //Ajouter un effet de hover sur les objets pour rétroaction 
+    private void OnMouseEnter()
     {
-        
+        AugmenterTaille(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnMouseExit()
     {
-        
+        AugmenterTaille(false);
+    }
+
+    private Vector3 tailleInitiale;
+    
+    private void Awake()
+    {
+        tailleInitiale = transform.localScale;
+    }
+
+    //Augmenter la taille
+    private void AugmenterTaille(bool status)
+    {
+        Vector3 tailleFinale = tailleInitiale;
+
+        //Si la condition est vraie, la taille augmente
+        if(status)
+            tailleFinale = tailleInitiale * 1.1f;
+            transform.localScale = tailleFinale;
+
+    // Update is called once per frame
+    // void Update()
+    // {
+    //     Sur clic de la souris sur l'objet 3D, il activera la scène correspondante.
+    //     if(Input.GetMouseButtonDown(0)){
+    //         Ray ray = GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+    //         RaycastHit hit;
+
+    //         if (Physics.Raycast(ray, out hit)){
+    //             Debug.Log("L'objet a été cliqué");
+    //         }
+    //     }
+    // }
     }
 }
