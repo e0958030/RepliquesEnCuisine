f975706959fbe1373a1344da36a66816ce5f5705
Camilia El Moustarih <e0958030@cmaisonneuve.qc.ca>
2025-02-12 16:47:10 -0500

    Elements 3D cliquables pour changer de scene fonctionnel

--- Assets/_MAIN/Scripts/CliquerObjet3D.cs
+++ Assets/_MAIN/Scripts/CliquerObjet3D.cs
@@ -2,10 +2,85 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class CliquerObjet3D : MonoBehaviour
 {
-    //Ajouter un effet de hover sur les objets pour rétroaction 
+    //Déclaration des variables
+
+    // Pour référer aux noms des scènes
+    public string nomSceneAppart = "NiveauIntro_Appart";
+
+    // Musique et effets sonores
+    private AudioSource audioSource;
+    public AudioClip sonClic; // Pour la rétroaction sur le clic de souris
+    private float delaiAvantChangement = 2f; // Délai de 2 secondes
+
+    // Lieux de la carte de la ville
+    public GameObject appartement;
+    public GameObject gare;
+    public GameObject ecole;
+    public GameObject epicerie;
+    public GameObject cafe;
+
+    void Start()
+    {
+        // Récupérer l'AudioSource attaché à l'objet
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        // Détecter que l'objet a été cliqué
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (appartement == GetClickedObject(out RaycastHit hit))
+            {
+                // Jouer le son du clic
+                if (audioSource && sonClic)
+                {
+                    audioSource.PlayOneShot(sonClic);
+                }
+
+                // Démarrer la coroutine pour attendre avant de changer de scène
+                StartCoroutine(AttendreEtChangerScene());
+            }
+        }
+    }
+
+    // Coroutine pour attendre avant de changer de scène
+    IEnumerator AttendreEtChangerScene()
+    {
+        yield return new WaitForSeconds(delaiAvantChangement);
+        SceneManager.LoadScene(nomSceneAppart);
+    }
+
+    // Code de détection de clic sur l'objet visé et non sur le canvas
+    GameObject GetClickedObject(out RaycastHit hit)
+    {
+        GameObject target = null;
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray.origin, ray.direction * 10, out hit))
+        {
+            if (!isPointerOverUIObject())
+            {
+                target = hit.collider.gameObject;
+            }
+        }
+        return target;
+    }
+
+    private bool isPointerOverUIObject()
+    {
+        PointerEventData ped = new PointerEventData(EventSystem.current);
+        ped.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(ped, results);
+        return results.Count > 0;
+    }
+
+    // Ajouter un effet de hover sur les objets lorsque la souris le survole
     private void OnMouseEnter()
     {
         AugmenterTaille(true);
@@ -17,34 +92,21 @@ public class CliquerObjet3D : MonoBehaviour
     }
 
     private Vector3 tailleInitiale;
-    
+
     private void Awake()
     {
         tailleInitiale = transform.localScale;
     }
 
-    //Augmenter la taille
+    // Augmenter la taille
     private void AugmenterTaille(bool status)
     {
         Vector3 tailleFinale = tailleInitiale;
 
-        //Si la condition est vraie, la taille augmente
-        if(status)
+        // Si la condition est vraie, la taille augmente
+        if (status)
             tailleFinale = tailleInitiale * 1.1f;
-            transform.localScale = tailleFinale;
 
-    // Update is called once per frame
-    // void Update()
-    // {
-    //     Sur clic de la souris sur l'objet 3D, il activera la scène correspondante.
-    //     if(Input.GetMouseButtonDown(0)){
-    //         Ray ray = GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
-    //         RaycastHit hit;
-
-    //         if (Physics.Raycast(ray, out hit)){
-    //             Debug.Log("L'objet a été cliqué");
-    //         }
-    //     }
-    // }
+        transform.localScale = tailleFinale;
     }
 }
