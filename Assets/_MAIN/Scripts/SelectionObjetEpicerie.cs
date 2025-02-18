0b72dec84f8e0ffd0833f71507064108ff5d5fec
Camilia El Moustarih <e0958030@cmaisonneuve.qc.ca>
2025-02-18 10:31:26 -0500

    Ajout d'un script pour les sons des items d'épicerie - fonctionnel

--- Assets/_MAIN/Scripts/SelectionObjetEpicerie.cs
+++ Assets/_MAIN/Scripts/SelectionObjetEpicerie.cs
@@ -6,9 +6,51 @@ using UnityEngine;
 //Script pour la sélection des objets de la liste d'épicerie
 public class SelectionObjetEpicerie : MonoBehaviour
 {
+    /////////// Déclaration des variables pour les sons des items d'épicerie ///////////
+    private AudioSource audioSource;
+
+    //Sons pour bon ou mauvais
+    public AudioClip sonMauvaisItem;
+    public AudioClip sonBonItem;
+
+    //Items de la liste d'épicerie
+    public AudioClip sonEpinards;
+    public AudioClip sonOeufs;
+    public AudioClip sonCreme;
+    public AudioClip sonFarine;
+    public AudioClip sonBeurre;
+    public AudioClip sonTomate;
+    public AudioClip sonSelPoivre;
+    public AudioClip sonCitrouille;
+    public AudioClip sonFromage;
+
     // Liste des tags des éléments de la liste d'épicerie
     // D'autres éléments seront rajoutés par la suite
-    public List<string> tagsObjets = new List<string> { "Oeufs", "Creme", "Epinards", "SelPoivre" }; 
+    public List<string> tagsObjets = new List<string> { "Epinards", "Oeufs", "Creme", "Farine", "Beurre", "Tomate", "SelPoivre", "Citrouille", "Fromage" };
+
+    //Dictionnaire contenant les données de la liste d'épicerie et les associe à un son
+    //Évite de faire plusieurs conditions if
+    private Dictionary<string, AudioClip> sonsParTag;
+
+    public void Start()
+    {
+        //Déclencher l'AudioSource sur ouverture du jeu
+        audioSource = GetComponent<AudioSource>();
+
+        //Associer chaque tag à son AudioClip avec le dictionnaire
+        sonsParTag = new Dictionary<string, AudioClip>
+        {   
+            { "Epinards", sonEpinards },
+            { "Oeufs", sonOeufs },
+            { "Creme", sonCreme },
+            { "Farine", sonFarine },
+            { "Beurre", sonBeurre },
+            { "Tomate", sonTomate },
+            { "SelPoivre", sonSelPoivre },
+            { "Citrouille", sonCitrouille },
+            { "Fromage", sonFromage }                    
+        };
+    }
 
     void Update()
     {
@@ -22,6 +64,24 @@ public class SelectionObjetEpicerie : MonoBehaviour
                 if (tagsObjets.Contains(hit.collider.tag)) // Vérifie si le tag est dans la liste
                 {
                     Destroy(hit.collider.gameObject);
+                }
+            }           
+        }
+
+        // Détection du survol de la souris
+        //Joue le son correspondant si le
+        Ray raySurvol = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitSurvol;
+
+        if (Physics.Raycast(raySurvol, out hitSurvol))
+        {
+            string tagSurvole = hitSurvol.collider.tag;
+
+            if (sonsParTag.ContainsKey(tagSurvole))
+            {
+                if (!audioSource.isPlaying)
+                {
+                    audioSource.PlayOneShot(sonsParTag[tagSurvole]);
                 }
             }
         }
